4903d5937752b549478069ab939a76096cbaeb40
fargol <ffereydounifargol@gmail.com>
2023-04-28 16:16:29 +0430

    add service modal added

--- untitled/src/Components/AddService/ImageSlider.jsx
+++ untitled/src/Components/AddService/ImageSlider.jsx
@@ -1,48 +1,5 @@
-// import React from 'react';
-
-// import {useState} from "react";
-// import axios from "axios";
-// import style from "./App.module.css";
-// import AddModal from "./AddModal";
-
-// //Axios config
-// axios.defaults.baseUrl = "";
-// axios.interceptors.request.use((request)=>{
-//   console.log(request);
-//   return request;
-// });
-// axios.interceptors.response.use((response)=>{
-//   console.log(response);
-//   return response.data;
-// });
-
-// const Add = () => {
-//   const[ShowComponent , setShowComponent] = useState({
-
-//     add: false,
-//     delete: false,
-//   });
-//   const btnhandler =(event) =>{
-//     setShowComponent({...ShowComponent, [event.target.name]: !ShowComponent[event.target.name],
-//     });
-//   };
-//   return (
-
-//     <div className = {style.mainContainer}>
-//       <div className = { style.btnComponentController}>
-//         <div style={{ display: "flex", marginLeft : "480PX", alignItems : "center", rowGap : "20px" }}>
-//           <button name = "add" onClick = {btnhandler}>
-//             add Services
-//           </button>
-//         </div>
-//       </div>
-//       {ShowComponent.add && <AddModal open={true}/>}
-//     </div>
-//   );
-// };
-
-// export default Add;
-
+import style from "./App.module.css";
+import AddModal from "./AddModal";
 import React, { useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
 // import "../css/ImageSlider.css";
@@ -60,6 +17,17 @@ function ImageSlider({ slides }, props) {
   const handleTabChange = (event, newIndex) => {
     setCurrentTabIndex(newIndex);
   };
+  const [ShowComponent, setShowComponent] = useState({
+    add: false,
+    delete: false,
+  });
+  const btnhandler = (event) => {
+    setShowComponent({
+      ...ShowComponent,
+      [event.target.name]: !ShowComponent[event.target.name],
+    });
+  };
+
   const [servicefront, setServicefront] = useState([]);
 
   let { id } = useParams();
@@ -93,58 +61,88 @@ function ImageSlider({ slides }, props) {
         }
       })
       .catch((err) => console.log(err));
+
+    //Axios config
+    axios.defaults.baseUrl = "";
+    axios.interceptors.request.use((request) => {
+      console.log(request);
+      return request;
+    });
+    axios.interceptors.response.use((response) => {
+      console.log(response);
+      return response.data;
+    });
   }, []);
   // useEffect(() => {
   //   console.log(servicefront);
   // }, [servicefront]);
 
   return (
-    <div className="wholebarber">
-      <div>
-        <div />
+    <>
+      <div className="wholebarber">
         <div>
-          <Tabs value={currentTabIndex} onChange={handleTabChange} centered>
-            {servicefront.map((item, index) => (
-              <Tab key={item.category} label={item.category} />
-            ))}
-          </Tabs>
-          {servicefront.map((item, index) => (
-            <div
-              key={item.category}
-              style={{ display: currentTabIndex === index ? "flex" : "none" }}
-            >
-              {item.categoryServices.map((x) => (
-                <Grid item xs={12} sm={6} md={4} key={x.service}>
-                  <Card
-                    sx={{
-                      maxWidth: 345,
-                      bgcolor: "#ffecee",
-                      fontFamily: "Roboto",
-                      color: "#120c1e",
-                      borderRadius: 3,
-                    }}
-                  >
-                    <CardMedia
-                      sx={{ height: 140 }}
-                      image="https://s2.uupload.ir/files/a9d966e052bdeb38027ca58ac3217845_z5j6.jpg"
-                      title="Hair style"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {x.service}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {x.price}$
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
+          <div />
+          <div>
+            <Tabs value={currentTabIndex} onChange={handleTabChange} centered>
+              {servicefront.map((item, index) => (
+                <Tab key={item.category} label={item.category} />
               ))}
-            </div>
-          ))}
+            </Tabs>
+            {servicefront.map((item, index) => (
+              <div
+                key={item.category}
+                style={{ display: currentTabIndex === index ? "flex" : "none" }}
+              >
+                {item.categoryServices.map((x) => (
+                  <Grid item xs={12} sm={6} md={4} key={x.service}>
+                    <Card
+                      sx={{
+                        maxWidth: 345,
+                        bgcolor: "#ffecee",
+                        fontFamily: "Roboto",
+                        color: "#120c1e",
+                        borderRadius: 3,
+                      }}
+                    >
+                      <CardMedia
+                        sx={{ height: 140 }}
+                        image="https://s2.uupload.ir/files/a9d966e052bdeb38027ca58ac3217845_z5j6.jpg"
+                        title="Hair style"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {x.service}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          {x.price}$
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      <div className={style.mainContainer}>
+        <div className={style.btnComponentController}>
+          <div
+            style={{
+              display: "flex",
+              marginLeft: "480PX",
+              alignItems: "center",
+              rowGap: "20px",
+            }}
+          >
+            <button name="add" onClick={btnhandler}>
+              add Services
+            </button>
+          </div>
+        </div>
+        {ShowComponent.add && <AddModal open={true} />}
+      </div>
+    </>
   );
 }
 
