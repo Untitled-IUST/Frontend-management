e2855fd04c82c6de7f8096c53d835e3ab851a479
Ehsan <ehsan.ahmadpoor2002@gmail.com>
2023-05-15 18:05:08 +0330

    merging add service

--- untitled/src/App.js
+++ untitled/src/App.js
@@ -2,6 +2,8 @@ import { Routes , Route } from 'react-router-dom';
 import LoginBarber from "./Pages/LoginSignUp/LoginBarber";
 import SignUpBarber from "./Pages/LoginSignUp/SignUpBarber";
 import OrderHistory from './Pages/OrderHistory/OrderHistory';
+import AddService from './Components/AddService/AddService';
+import SideBar from './Components/SideBar/SideBar';
 
 function App() {
   
@@ -9,8 +11,18 @@ function App() {
     <Routes>
       <Route path="/" Component={LoginBarber}/>
       <Route path="/SignUpBarber" Component={SignUpBarber} />
-      <Route path="/OrderHistory" Component={OrderHistory}/>
-      <Route path="/AddService" />
+      <Route path="/OrderHistory" element={
+        <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
+          <SideBar/>
+          <OrderHistory/>
+        </div>
+      }/>
+      <Route path="/AddService" element={
+        <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
+          <SideBar/>
+          <AddService/>
+        </div>
+      }/>
     </Routes>
   );
 }
--- untitled/src/Components/SideBar/SideBar.jsx
+++ untitled/src/Components/SideBar/SideBar.jsx
@@ -27,19 +27,18 @@ function SideBar(){
     })},[])
 
     return (
-      <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
         <Sidebar backgroundColor="#261B39"  style={{ height: "100vh" }} >
           <Menu rootStyles={{color: "#fecbca"}} >
             <MenuItem icon={<MenuOutlinedIcon />} onClick={() => { collapseSidebar(); }} style={{ textAlign: "center" }} >
               {" "}
               <h2>{userName}</h2>
             </MenuItem>
-            <MenuItem icon={<StorefrontIcon/>} component={<Link to="/AddService" />}>Profile</MenuItem>
-            <MenuItem icon={<ManageHistoryIcon/>} component={<Link to="/OrderHistory" />}>Hairdresser</MenuItem>
-            <MenuItem icon={<LogoutIcon />} onClick={() => {localStorage.removeItem('accessTokenBarber')}} component={<Link to="/login" />}>Log Out</MenuItem>
+            <MenuItem icon={<StorefrontIcon/>} component={<Link to="/AddService" />}>Manage Services</MenuItem>
+            <MenuItem icon={<ManageHistoryIcon/>} component={<Link to="/OrderHistory" />}>Orders History</MenuItem>
+            <MenuItem icon={<LogoutIcon />} onClick={() => {localStorage.removeItem('accessTokenBarber')}} component={<Link to="/" />}>Log Out</MenuItem>
           </Menu>
         </Sidebar>
-      </div>
     )
 
 }
+export default SideBar;
